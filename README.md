12b0adbdffadea48f6b59035a03022c8e8900ee3
Henry Lee <henryleeworld@gmail.com>
2024-08-03 00:08:02 +0800

    feat:升級至 laravel 11

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Laravel 10 Eloquent 模型陣列驅動
+# Laravel 11 Eloquent 模型陣列驅動
 
 引入 calebporzio 的 sushi 套件來擴增 Eloquent 模型陣列驅動，允許從資料庫以外的資料來源建立 Eloquent 模型來查詢提供的已寫死陣列資料。
 
 ## 使用方式
+- 打開 php.ini 檔案，啟用 PHP 擴充模組 sqlite3，並重啟服務器。
 - 把整個專案複製一份到你的電腦裡，這裡指的「內容」不是只有檔案，而是指所有整個專案的歷史紀錄、分支、標籤等內容都會複製一份下來。
 ```sh
 $ git clone
@@ -17,10 +18,10 @@ $ composer install
 $ php artisan key:generate
 ```
 - 在瀏覽器中輸入已定義的路由 URL 來訪問，例如：http://127.0.0.1:8000。
-- 你可以經由 `/users/{名稱}` 來進行使用者查詢。
+- 你可以經由 `/users/{姓名}` 來進行使用者查詢。
 
 ----
 
 ## 畫面截圖
-![](https://i.imgur.com/1zNonMb.png)
+![](https://i.imgur.com/jIscePU.png)
 > 可以從已寫死陣列中取得資料
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,8 +11,8 @@ class UsersController extends Controller
 {
     public function show(string $name) 
     {
-        echo '查詢名稱：' . $name . PHP_EOL;
+        echo __('Query name:') . $name . PHP_EOL;
         $user = User::whereName($name)->first();
-        echo '符合查詢名稱第一筆資料的名稱：「' . $user->name . '」、聲優：「' . $user->cv . '」、所屬宿舍：「' . $user->affiliation . '」、機體：「' . $user->mobile_suit . '」、資料：「' . $user->data . '」' . PHP_EOL;  
+        echo __('The name that matches the first data of the query name: "') . $user->name . __('", character voice: "') . $user->cv . __('", affiliation: "') . $user->affiliation . __('", mobile suit: "') . $user->mobile_suit . __('", data: "') . $user->data . __('"') . PHP_EOL;  
     }
 }
